3b681cc3310a40bf759a0b39c4a5278c36309a51
Johan Mazzo <johiny7@live.com>
2024-11-16 19:16:06 -0500

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,6 +4,10 @@ import { Link } from "react-router-dom";
 function Home() {
   return (
     <>
+<h1>Picas y Fijas, Hot And Cold
+</br>
+Johan Sebastian Castillo Mazo
+</h1>
       <div className="card-container">
         <Link to="/picasandfijas" className="card">
           <button>Ir a Picas y Fijas</button>
