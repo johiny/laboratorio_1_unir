4d8d0e531b6c1dd7e58d50341079589a7d078856
Johan Mazzo <johiny7@live.com>
2024-11-16 19:18:26 -0500

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,9 +5,8 @@ function Home() {
   return (
     <>
 <h1>Picas y Fijas, Hot And Cold
-</br>
-Johan Sebastian Castillo Mazo
 </h1>
+<h2>Johan Sebastian Castillo Mazo</h2>
       <div className="card-container">
         <Link to="/picasandfijas" className="card">
           <button>Ir a Picas y Fijas</button>
